8149a1a6ed6778aac54c444308cb4362a8141f08
Damian Stepniak <95021613456@st.wns.uph.edu.pl>
2018-04-21 13:14:13 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.result = [];
+app.listen(8085, function () {
+    console.log('nasluchuje na 8085');
+});
+
+app.get('/', (req, resp) => {
+    try {
+        var data1 = fs.readFileSync('./mojewiadomosci.json');
+        app.result = JSON.parse(data1);
+    } catch (e) {
+        app.result = [];
+    }
+    
+    resp.render('index.ejs', { wiadomosci: app.result });
+});
+
+app.post('/wiadomosci', (req, resp) => {
+    app.result.push({
+        "imie": "" + req.body.imie + "",
+        "nazwisko": "" + req.body.nazwisko + "",
+        "wiadomosc": "" + req.body.wiadomosc + ""
+    });
+    fs.writeFileSync('./mojewiadomosci.json', JSON.stringify(app.result));
+
+    resp.redirect('/');
+});
+
+app.get('/wyczysc', (req, resp) => {
+    fs.writeFileSync('./mojewiadomosci.json', '');
+
+    resp.redirect('/');
+});
+
